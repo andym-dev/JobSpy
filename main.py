bfd9eb3e81ad9259ce88d6f9f1f63139fda9b86f
andym-dev <basetankrs@gmail.com>
2023-11-09 16:17:03 +0000

    Datetime files and gitignore csv

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 # pip install python-jobspy
 
 from jobspy import scrape_jobs
+from datetime import datetime
+
+job_title_to_search = 'Head of Procurement'
 
 jobs = scrape_jobs(
     site_name=["indeed", "linkedin", "glassdoor"],
-    search_term="Brand Manager",
+    search_term=job_title_to_search,
     location="London, UK",
-    results_wanted=10,
+    results_wanted=30,
     country_indeed='UK'  # only needed for indeed / glassdoor
 )
+
+# get current date and time
+current_datetime = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+
 print(f"Found {len(jobs)} jobs")
 print(jobs.head())
-jobs.to_csv("jobs.csv", index=False) # to_xlsx
-
+jobs.to_csv(f"{job_title_to_search} __ {current_datetime} .csv", index=False) # to_xlsx
